2b22c3ab552b5d07d72e13a061dee1c6fae7743a
thwjd9393 <thwjd9393@naver.com>
2024-03-26 11:51:02 +0900

    update) 메인화면 제작, 운동 이미지 추가, 바인딩 설정

--- MyExerciseApp/app/src/main/java/com/mbsysoft/myexerciseapp/MainActivity.kt
+++ MyExerciseApp/app/src/main/java/com/mbsysoft/myexerciseapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.mbsysoft.myexerciseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.mbsysoft.myexerciseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+    //상호 연결되는 화면만 참조하기 때문에 안전함
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(this, "시직", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //binding 을 사용 할 땐 onDestroy에서 binding = null로 재설정!!!
+
+        binding = null //메모리 누수 방지
+    }
+
 }
