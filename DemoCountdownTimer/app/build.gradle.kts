585272e7e9cdf4447c5fb62265bcf8f54d5fac35
thwjd9393 <thwjd9393@naver.com>
2024-03-26 15:18:25 +0900

    update) 타이머 데모

--- DemoCountdownTimer/app/build.gradle.kts
+++ DemoCountdownTimer/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mbsysoft.democountdowntimer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mbsysoft.democountdowntimer"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- DemoCountdownTimer/app/src/main/java/com/mbsysoft/democountdowntimer/MainActivity.kt
+++ DemoCountdownTimer/app/src/main/java/com/mbsysoft/democountdowntimer/MainActivity.kt
@@ -2,10 +2,64 @@ package com.mbsysoft.democountdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.mbsysoft.democountdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
+    private var countDownTimer: CountDownTimer? = null
+    private var timerDuration:Long = 60000
+    // pauseOffset = timerDuration - time
+    private var pauseOffset:Long = 0 //일시 정지 후 다시 시작하는 경우를 대비한 변수
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.tvTimer?.text = "${(timerDuration/1000).toString()}"
+
+        binding?.btnStart?.setOnClickListener {
+            startTimer(pauseOffset)
+        }
+
+        binding!!.btnPause.setOnClickListener {
+            pauseTitme()
+        }
+
+        binding!!.btnReset.setOnClickListener {
+            resetTimer()
+        }
+    }
+
+    private fun startTimer(mPauseOffsetL: Long) {
+        countDownTimer = object : CountDownTimer(timerDuration-mPauseOffsetL, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                pauseOffset = timerDuration - millisUntilFinished
+                binding!!.tvTimer.text = (millisUntilFinished/1000).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity, "Timer is Finished", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun pauseTitme() {
+        if (countDownTimer != null) {
+            countDownTimer!!.cancel()
+        }
+    }
+
+    private fun resetTimer() {
+        if (countDownTimer != null) {
+            countDownTimer!!.cancel()
+            binding!!.tvTimer.text = "${(timerDuration/1000).toString()}"
+            countDownTimer = null
+            pauseOffset = 0
+        }
     }
 }
