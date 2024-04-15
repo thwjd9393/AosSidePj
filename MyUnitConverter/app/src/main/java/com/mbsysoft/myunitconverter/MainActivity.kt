f60b386c9c20e56a01fb534907d977135a41ca2b
thwjd9393 <thwjd9393@naver.com>
2024-04-15 17:17:21 +0900

    create) text 행과 열

--- MyUnitConverter/app/src/main/java/com/mbsysoft/myunitconverter/MainActivity.kt
+++ MyUnitConverter/app/src/main/java/com/mbsysoft/myunitconverter/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mbsysoft.myunitconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,15 +18,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyUnitConverterTheme {
+            MyUnitConverterTheme { //적용할 테마 불러오는 곳
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(), //fillMaxSize => 화면 전체 채우기
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+//                    Greeting("Android")
+//                    함수를 두번 호출해도 Hello Android는 UI에 한번만 표시됨
+//                    열을 알려주지않으면 내가 어디에 위치해야되는지 몰라서 자리를 찾지못한다.
+
+                    UnitConverter() // @Compose의 Column과 row를 사용하여 데이터를 쌓는다
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun UnitConverter() {
+    Column {
+        // ui 요소 쌓는 방법
+        // 세로로 표시 (열)
+        Greeting("aaa")
+        Greeting("bbb")
+        Row { // 가로로 표시 (행)
+            Greeting("Android1")
+            Greeting("Android2")
         }
     }
 }
@@ -33,7 +54,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
