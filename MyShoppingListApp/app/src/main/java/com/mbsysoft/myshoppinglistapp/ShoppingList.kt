cde285d5bfbec4671fefa70183c9e3f72b1e4bfc
thwjd9393 <thwjd9393@naver.com>
2024-04-19 13:32:15 +0900

    update) 알럿 추가

--- MyShoppingListApp/app/src/main/java/com/mbsysoft/myshoppinglistapp/ShoppingList.kt
+++ MyShoppingListApp/app/src/main/java/com/mbsysoft/myshoppinglistapp/ShoppingList.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -29,12 +31,16 @@ fun ShoppingListApp() {
         mutableStateOf(listOf<ShoppingItem>())
     }
 
+    var showDialog by remember {
+        mutableStateOf(false) //표시 상태 지켜보는 애 true가 되면 보임
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text(text = "Add Item")
@@ -50,4 +56,17 @@ fun ShoppingListApp() {
                 }
             })
     }
+
+    //다이아 로그
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "추가") },
+            text = { Text(text = "추가 항목") },
+            confirmButton = { 
+                Text(text = "확인")
+            })
+    }
 }
+
+
