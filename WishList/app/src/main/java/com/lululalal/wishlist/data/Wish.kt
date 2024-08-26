3a79eb1a7b96a92468d8d74885c0dfebbde8aef7
thwjd9393 <thwjd9393@naver.com>
2024-08-26 16:51:38 +0900

    update) 엔티티 작성 & dao 작성(진행중)

--- WishList/app/src/main/java/com/lululalal/wishlist/data/Wish.kt
+++ WishList/app/src/main/java/com/lululalal/wishlist/data/Wish.kt
@@ -1,8 +1,16 @@
 package com.lululalal.wishlist.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id : Long =0L,
+    @ColumnInfo(name = "wish-title")
     val title:String,
+    @ColumnInfo("wish-desc")
     val description:String
 )
 
---
+++ WishList/app/src/main/java/com/lululalal/wishlist/data/WishDao.kt
@@ -0,0 +1,6 @@
+package com.lululalal.wishlist.data
+
+//추상 클래스로 작성
+//메소드 이름만 있는 거
+abstract class WishDao {
+}
